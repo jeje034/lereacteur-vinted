9fcb7354bba23c8b0b6c1699fe665f3463c0fe67
Jeje S <54934028+jeje034@users.noreply.github.com>
2021-04-06 18:30:14 +0200

    Contrôle des paramètres offer : contrôles complétés + interface au lieu de any pour la requête.

--- middlewares/are-offer-parameters-ok.ts
+++ middlewares/are-offer-parameters-ok.ts
@@ -1,6 +1,18 @@
 import { NextFunction, Response } from "express";
 
-const areOfferParametersOK = (req: any, res: Response, next: NextFunction) => {
+interface iOfferParametersToControl {
+    fields: {
+        title: string;
+        description: string;
+        price: number;
+    };
+}
+
+const areOfferParametersOK = (
+    req: iOfferParametersToControl,
+    res: Response,
+    next: NextFunction
+) => {
     if (req.fields.title && req.fields.title.length > 50) {
         res.status(400).json({
             error: { message: "Le titre est limité à 50 caractères." },
@@ -16,8 +28,27 @@ const areOfferParametersOK = (req: any, res: Response, next: NextFunction) => {
     }
 
     if (req.fields.price) {
-        let price = Number.parseInt(req.fields.price);
-        if (!isNaN(price) && price > 100000) {
+        let price = req.fields.price;
+
+        if (isNaN(price)) {
+            res.status(400).json({
+                error: {
+                    message: "Le prix n'est pas un nombre correct.",
+                },
+            });
+            return;
+        }
+
+        if (price < 0) {
+            res.status(400).json({
+                error: {
+                    message: "Un prix ne peut pas être négatif.",
+                },
+            });
+            return;
+        }
+
+        if (price > 100000) {
             res.status(400).json({
                 error: {
                     message: "Le prix est limité à 100000 €.",
